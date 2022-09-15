ff8130f0067e3b40dd37e4161a94fef2025f07f1
bia9400 <ferro.biagio@gmail.com>
2022-09-15 18:49:22 +0200

    Eloquent Relationship

--- app/Accommodation.php
+++ app/Accommodation.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accommodation extends Model
 {
-    //
+    public function views(){
+        return $this->hasMany("App\View");
+    }
+    public function messages(){
+        return $this->hasMany("App\Message");
+    }
+    public function typology(){
+        return $this->belongsTo("App\Typology");
+    }
+    public function services(){
+        return $this->belongsToMany("App\Service");
+    }
+    public function user(){
+        return $this->belongsTo("App\User");
+    }
+    public function sponsorships(){
+        return $this->belongsToMany("App\Sponsorship");
+    }
+
 }
