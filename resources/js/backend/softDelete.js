881926da57c92319592fd3f0cb733189e73dd1dc
bia9400 <ferro.biagio@gmail.com>
2022-09-21 15:43:45 +0200

    jbdwq

--- resources/js/backend/softDelete.js
+++ resources/js/backend/softDelete.js
@@ -1,4 +1,4 @@
-// Questa funzione aspetta che tutto il contenuto venga caricato
+/* // Questa funzione aspetta che tutto il contenuto venga caricato
     window.addEventListener("DOMContentLoaded", function () {
         
         // Crea un array con tutti i form della pagina con classe 'form-delete'
@@ -14,15 +14,15 @@
           e.preventDefault();
             
                 // Crea un alert e lo salva in una variabile
-          /* const result = confirm("Sei sicuro di voler cancellare questo elemento?")
+          const result = confirm("Sei sicuro di voler cancellare questo elemento?")
                 
                 // Se valido l'alert invoca la funzione submit sul form
           if (result) {
                 form.submit();
-          } */
+          }
     
         })
     
       }
     
-    })
+    }) */
