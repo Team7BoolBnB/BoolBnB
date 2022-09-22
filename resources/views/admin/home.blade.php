16ddb7bf4df19b53472375899f1505c1c452cf64
meta-ela <anxhelameta@live.it>
2022-09-22 14:39:34 +0200

    add card fixed details

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -21,9 +21,11 @@
                 @foreach ($accommodations as $accommodation)
                     <div class="col-sm-5">
                         <div class="card_admin">
-                            <div class="available_info">
-                                <span>Available</span>
-                            </div>
+                            @if($accommodation->available == "0")
+                                <h6 class=" badge not_available_info">Not Visible</h6>
+                            @else
+                                <h6 class=" badge available_info">Visible</h6>
+                            @endif
                             <div class="accommodation_info">
                                 <div class="fs-3">{{ $accommodation->title }}</div>
                                 <div>{{ $accommodation->address }}</div>
@@ -135,9 +137,11 @@
                             
                             <div class="col-sm-5">
                                 <div class="card_admin">
-                                    <div class="available_info">
-                                        <span>Active</span>
-                                    </div>
+                                    @if($accommodation->available == "0")
+                                        <h6 class=" badge not_available_info">Not Visible</h6>
+                                    @else
+                                        <h6 class=" badge available_info">Visible</h6>
+                                    @endif
                                     <div class="accommodation_info">
                                         <div class="fs-3">{{ $accommodation->title }}</div>
                                         <div>{{ $accommodation->address }}</div>
