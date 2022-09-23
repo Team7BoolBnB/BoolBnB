dca58fc5754ca67183d9e0142d4d28f041f77017
bia9400 <ferro.biagio@gmail.com>
2022-09-23 15:09:12 +0200

    modfy

--- resources/js/view/Home.vue
+++ resources/js/view/Home.vue
@@ -1,58 +1,60 @@
 <template>
   <div>
-   
+      <NavBar></NavBar>
+  
+      <!-- sezione con carosello di immagini -->
+      <div class="debug_carosel">
+          <div class="heroOverlay">
+              <div class="h-100 d-flex justify-content-center align-items-center">
+                  <div class="container_link_advanced d-flex flex-column justify-content-center align-items-center">
+                      <h3>Ready for your next trip?</h3>
+                      <p class="text-center py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                      <router-link :to="{ name: 'advanced-search' }" class="basicBtn bigBtn primaryBtn">Start search</router-link>
+                  </div>
+              </div>
+          </div>
+      </div>
+  
+      <!-- sezione main con card => chiamata api -->
+      <div class="container py-5">
+          <h2 class="text-center pt-3 pb-5">Sponsorized Accommodations</h2>
+          <div class="row">
+              <div class="col-3">
+                  <CardItem></CardItem>
+              </div>
+          </div>
+      </div>
+  
   </div>
-
-
-
-</template>
-
-<script>
-import axios from "axios";
-import NavBar from "../components/NavBar.vue";
-import CardList from "../components/CardList.vue";
-
-export default {
-  components: { NavBar, CardList },
-  data() {
-    return {
-      accommodations: [],
+  
+  </template>
+  
+  <script>
+    import axios from "axios";
+    import NavBar from "../components/NavBar.vue";
+    import CardItem from "../components/CardItem.vue";
+   
+    
+    export default {
+      components: { NavBar,  },
+      data() {
+        return {
+          accommodations: [],
+        };
+      },
+      methods:{
+        async fetchdata(){
+           await  axios.get("/api/accommodations").then((resp)=>{
+                this.accommodations=resp.data;
+            })
+        }
+      },
+       mounted() {
+        this.fetchdata();
+      },
     };
-  },
-  methods:{
-    async fetchdata(){
-       await  axios.get("/api/accommodations").then((resp)=>{
-            this.accommodations=resp.data;
-        })
-    }
-  },
-   mounted() {
-    this.fetchdata();
-  },
-};
-</script>
-
-
-<style lang="scss" scoped>
-@import "../../sass/partials/variables";
-
-.debug_carosel {
-  height: 500px;
-  background-color: rgb(157, 107, 130);
-  background-image: url("../../../public/img/img-prova.jpg");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-
-.container_link_advanced {
-  background-color: $primaryColor;
-  height: 300px;
-  width: 400px;
-  border-radius: 1rem;
-  border: 2px solid $primaryColor;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+    </script>
+    
+  
+  
+  
