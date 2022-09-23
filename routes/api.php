ddd27463116c4c1354a479f2e75c4a226585964c
Luchito37 <lucamatteotti01@gmail.com>
2022-09-23 12:25:33 +0200

    aggiunto api controller e rotte

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get("/sponsorship","Api\PaymentController@generate");
 Route::post("/make/payment","Api\PaymentController@makePayment");
+
+Route::get("/accommodations","Api\HomeController@index");
