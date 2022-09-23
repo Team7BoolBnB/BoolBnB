24aae674b6566e6e6f87eaeca7d0934abae622a6
bia9400 <ferro.biagio@gmail.com>
2022-09-23 13:06:47 +0200

    wewqef

--- resources/js/view/AdvancedSearch.vue
+++ resources/js/view/AdvancedSearch.vue
@@ -4,9 +4,29 @@
     </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script>
+    import axios from "axios";
+    
+    
+    export default {
+      
+      data() {
+        return {
+          data: [],
+        };
+      },
+      methods:{
+        async fetchdata(){
+           await  axios.get("/api/advancedsearch").then((resp)=>{
+                this.data=resp.data;
+            })
+        }
+      },
+       mounted() {
+        this.fetchdata();
+      },
+    };
+    </script>
 
 <style scoped>
 
